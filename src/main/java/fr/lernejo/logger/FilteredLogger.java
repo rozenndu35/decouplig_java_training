3b571bd247f1f832e44d8311a9dcb65ed3a5f0fa
Costiou Rozenn <rozenn.costiou@orange.fr>
2021-12-01 13:32:27 +0100

    filtre de logger

---
+++ src/main/java/fr/lernejo/logger/FilteredLogger.java
@@ -0,0 +1,27 @@
+package fr.lernejo;
+
+import java.util.function.Predicate;
+
+import fr.lernejo.logger.Logger;
+
+public class FilteredLogger implements Logger{
+
+	private Logger delegate;
+	private Predicate<String> condition;
+	private String name;
+	
+	public FilteredLogger(Logger delegate, Predicate<String> condition, String name) {
+		  this.condition = condition;
+		  this.delegate = delegate;
+		  this.name = name;
+		}
+	
+	@Override
+	public void log(String message) {
+		if (this.condition.test(this.name)) {
+			this.delegate.log(message);
+		}
+		
+	}
+
+}
--- src/main/java/fr/lernejo/logger/LoggerFactory.java
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -1,10 +1,16 @@
 package fr.lernejo;
 
+import java.util.function.Predicate;
+
 import fr.lernejo.logger.ContextualLogger;
 import fr.lernejo.logger.Logger;
 
 public class LoggerFactory {
     public static Logger getLogger(String name){
-        return new CompositeLogger(new ContextualLogger(name , new FileLogger("../LoggerApp.txt")), new ContextualLogger(name , new ConsoleLogger())) ;
+        Predicate<String> condition = message -> message.contains("simulation");
+        ContextualLogger logerContextFile = new ContextualLogger(name , new FileLogger("../LoggerApp.txt"));
+        ContextualLogger logerContextConsol = new ContextualLogger(name , new ConsoleLogger());
+        
+        return new CompositeLogger(new FilteredLogger(logerContextFile, condition, name), logerContextConsol) ;
     }
 }
