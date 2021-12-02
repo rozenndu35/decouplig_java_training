9c752a45da40225da07faea5804892b116297127
Costiou Rozenn <rozenn.costiou@orange.fr>
2021-12-02 15:32:58 +0100

    ejout de log

--- src/main/java/fr/lernejo/logger/LoggerFactory.java
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -10,7 +10,7 @@ import fr.lernejo.logger.Logger;
 public class LoggerFactory {
     public static Logger getLogger(String name){
         Predicate<String> condition = message -> message.contains("simulation");
-        Logger fileLogger = new ContextualLogger(name, new FileLogger("../LoggerApp.txt"));
+        Logger fileLogger = new ContextualLogger(name, new FileLogger("../log.txt"));
         Logger consoleLogger = new ContextualLogger(name , new FilteredLogger(new ConsoleLogger(), condition));
         
         return new CompositeLogger(fileLogger, consoleLogger) ;
